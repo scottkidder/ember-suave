a72af0785d3872fa7aad9cd0bc383a3bd01587b5
Scott Kidder <scott.kidder@ilq.com>
2015-09-30 00:12:46 -0400

    Don't use the jscsrc-builder, we are going to use the preset functionality from jscs.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /* jshint node: true */
 'use strict';
 var JSCSFilter = require('broccoli-jscs');
-var jscsrcBuilder = require('./lib/jscsrc-builder');
 
 module.exports = {
   name: 'ember-suave',
 
   lintTree: function (type, tree) {
     var jscsOptions = this.app.options.jscsOptions || {};
-    jscsOptions.configPath = jscsrcBuilder(this.project);
+    if (jscsOptions.configPath === undefined) {
+      jscsOptions.configPath = '.jscsrc';
+    }
     return new JSCSFilter(tree, jscsOptions);
   },
 
